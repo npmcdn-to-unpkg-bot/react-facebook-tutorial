ba94b69960a7e5c95cddd1b4169fa44bfe2f3654
Monica Mow <mow.monica@gmail.com>
2016-08-24 18:54:14 -0700

    Add Markdown to format your text inline (remarkable library)

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -39,12 +39,14 @@ var CommentForm = React.createClass({
 // Comment is child of CommentList
 var Comment = React.createClass({
   render: function() {
+    // tutorial 6 (add Markdown)
+    var md = new Remarkable();
     return(
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        {md.render(this.props.children.toString())}
       </div>
     );
   }
