6482b4ea47fa6508b17b5f195c30a3eb345067b2
Monica Mow <mow.monica@gmail.com>
2016-08-27 15:50:02 -0700

    Allow component to update state with componentDidMount

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,9 +1,22 @@
 // tutorial 1
 var CommentBox = React.createClass({
+  // tutorial 12
   getInitialState: function() {
     return {data: []};
   },
-
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
     // tutorial 3/9
