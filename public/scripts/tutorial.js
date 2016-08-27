0fa5f3b995ba635ae5e8c899d3be09f154c011fe
Monica Mow <mow.monica@gmail.com>
2016-08-27 15:33:38 -0700

    Set up initial state (empty data array)

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,11 +1,15 @@
 // tutorial 1
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+
   render: function() {
     return (
     // tutorial 3/9
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
